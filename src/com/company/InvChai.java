0f1cc5fa40a4719d14217a72c8bc4737b369c7ca
Ludovic SOUPLET <ludovic.s@protonmail.com>
2019-02-04 16:22:37 +0100

    Ajout code pour l'inversion de chaine + debut palindrome

--- src/com/company/InvChai.java
+++ src/com/company/InvChai.java
@@ -1,9 +1,49 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class InvChai {
     public void invChai(){
+
+        Scanner sc = new Scanner(System.in);
+        String string1;
+        String string2 = "";
+        System.out.println("Entrez un mot:");
+
+        string1 = sc.next();
+        for(int i = string1.length() - 1; i>= 0; i--)
+        {
+            string2 = string2 + string1.charAt(i);
+        }
+
+        System.out.println("Le mot " + string1 + " inversé est "+ string2);
+
+
+        /*
+        //Initialisation des variable
+        Scanner sc = new Scanner(System.in);
+        String string1;
+        String string2;
+        int string1Length;
+        int i = 0;
+
+        //Création d'un tableau de la longueur de la chaine de caractère
+        System.out.println("Bonjour,veuillez saisir un mot:");
+        string1 = sc.next();
+        string1Length = string1.length();
+        System.out.println(string1Length); // a effacer
+        char string1Tab[] = new char[string1Length];
+        System.out.println(string1Tab[5]); // a effacer
+
+        while(i < string1Length)
+            string1Tab[i] = string1.charAt(i);
+
+        System.out.println("3");
+        System.out.println(string1Tab);
         //recuper une chaine de carac
         //la spliter et la stocker dans un tableau (d'une longueur String.lenght)
         //concatenner chaque valeur du tableau en le déroulant depuis la fin dans une nouvelle variable
+        */
+
     }
 }
--- src/com/company/Palin.java
+++ src/com/company/Palin.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Palin {
     public void palin(){
-        //quasi identique a invChaine
-        //faire rentrer un booleen en jeux
+        Scanner sc = new Scanner(System.in);
+        String string1;
+        String string2 = "";
+        boolean palin ;
+        System.out.println("Entrez un mot:");
+
+        string1 = sc.next();
+        for(int i = string1.length() - 1; i>= 0; i--)
+        {
+            string2 = string2 + string1.charAt(i);
+        }
+
+
+        if (string1 == string2) {
+            palin = true;
+            System.out.println("True");
+        }
+        else {
+            palin = false;
+            System.out.println("False");
+        }
     }
 }
