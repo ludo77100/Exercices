a7a9268c5df47478312bc3c43ab85dc6c367cead
Ludovic SOUPLET <ludovic.s@protonmail.com>
2019-02-04 14:38:49 +0100

    Ajout du code dans la classe FirstOcc

--- src/com/company/FirstOcc.java
+++ src/com/company/FirstOcc.java
@@ -1,7 +1,25 @@
 package com.company;
 
-public class FirstOcc {
-    public void firstOcc() {
+import java.util.Scanner;
 
+public class FirstOcc {
+    private void rechercheTab() {
+        //coder pour passer en paramètres
     }
-}
+        public void firstOcc () {
+            int ages[] = {5, 18, 16, 17, 25, 32, 28, 45, 50, 70};
+            int i = 0;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bonjour, veuillez saisir un nombre svp");
+            int nombre = sc.nextInt();
+            while (i < ages.length && nombre != ages[i])
+                i++;
+            if (i < 10) {
+                i = i + 1;
+                System.out.println("Le nombre " + nombre + " est dans le tableau en position " + i);
+            } else {
+                System.out.println("-1");
+            }
+        }
+    }
+
